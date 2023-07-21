a416764e308ec850d4e871c26fa3e308fecb706d
jjsair0412 <jjsair0412@naver.com>
2023-07-22 01:03:25 +0900

    init Kafka Opensearch Project

---
+++ src/main/java/com/example/kafkaopensearchproject/KafkaOpensearchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaopensearchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOpensearchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOpensearchProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaopensearchproject/KafkaOpensearchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaopensearchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOpensearchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
